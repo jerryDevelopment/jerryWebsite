cc0acd1a6412aae2ecd45e8d4b9fd88d81012b6a
Sebastian Kempf <WORK@iMac-3.fritz.box>
2018-06-04 17:03:49 +0200

    make it better

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -12,16 +12,21 @@ import axios from 'axios'
 export default {
   components: {  },
   asyncData ({ params }) {
-    return axios.get('http://localhost/playground/nuxtjs/rest-api/about-page/')
-    .then((res) => {
-      return { 
-        name: res.data.name, 
-        text: res.data.text 
-      }
-    })
-    .catch((e) => {
-      error({ statusCode: 404, message: 'Post not found' })
-    })
+    // return axios.get('http://localhost/playground/nuxtjs/rest-api/about-page/')
+    // .then((res) => {
+    //   return { 
+    //     name: res.data.name, 
+    //     text: res.data.text 
+    //   }
+    // })
+    // .catch((e) => {
+    //   error({ statusCode: 404, message: 'Post not found' })
+    // })
+    return { 
+      name: 'About', 
+      text: 'This is the about page.'
+    }
+
   }
 }
 
