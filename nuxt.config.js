6eb340dac567a44df308384b8b250d22bbbbb6fe
SebastianKempf <sk@tilgner-kempf.com>
2020-06-29 16:10:24 +0200

    bleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,51 +73,52 @@ module.exports = {
     { src: '~/plugins/vue-instagram', ssr: false }
   ],
   modules: [
+    '@nuxtjs/markdownit',
     ['nuxt-cookie-control', {
       //your options
     }]
   ],
-cookies: {
-  necessary: [
-    {
-      //if multilanguage
-      name: {
-        en: 'Default Cookies'
-      },
-      //else
-      name:  'Default Cookies',
-      //if multilanguage
-      description: {
-        en:  'Used for cookie control.'
-      },
-      //else
-      description:  'Used for cookie control.',
-      cookies: ['cookie_control_consent', 'cookie_control_enabled_cookies']
-    }
-  ],
-  optional: [
-    {
-      name:  'Google Analitycs',
-      //if multilanguage
-      description: {
-        en:  'Google GTM is ...'
-      },
-      //else
-      description:  'Google GTM is...',
-      src:  'https://www.googletagmanager.com/gtag/js?id=<API-KEY>',
-      async:  true,
-      cookies: ['_ga', '_gat', '_gid'],
-      accepted: () =>{
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js'
-        });
-      },
-      declined: () =>{
+  cookies: {
+    necessary: [
+      {
+        //if multilanguage
+        name: {
+          en: 'Default Cookies'
+        },
+        //else
+        name:  'Default Cookies',
+        //if multilanguage
+        description: {
+          en:  'Used for cookie control.'
+        },
+        //else
+        description:  'Used for cookie control.',
+        cookies: ['cookie_control_consent', 'cookie_control_enabled_cookies']
       }
-    }
-  ]
-}
+    ],
+    optional: [
+      {
+        name:  'Google Analitycs',
+        //if multilanguage
+        description: {
+          en:  'Google GTM is ...'
+        },
+        //else
+        description:  'Google GTM is...',
+        src:  'https://www.googletagmanager.com/gtag/js?id=<API-KEY>',
+        async:  true,
+        cookies: ['_ga', '_gat', '_gid'],
+        accepted: () =>{
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+          });
+        },
+        declined: () =>{
+        }
+      }
+    ]
+  }
 
 }
